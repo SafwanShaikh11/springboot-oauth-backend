bc09e2a65013b39e80690abd98f33db163617824
Safwan Shaikh <walterwhte112@gmail.com>
2026-06-17 15:37:38 +0530

    Refactor UserController and migrate to PostgreSQL

--- demo/src/main/java/com/example/demo/UserController.java
+++ demo/src/main/java/com/example/demo/UserController.java
@@ -5,18 +5,20 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import java.util.List;
+
+
 @RestController
-public class me {
+public class UserController {
 
     private final UserRepository userRepository;
 
-    public me(UserRepository userRepository) {
+    public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
 
-    @GetMapping("/Me")
-    public User Me(@AuthenticationPrincipal OAuth2User user) {
+    @GetMapping("/me")
+    public User getCurrentUser(@AuthenticationPrincipal OAuth2User user) {
 
         String email = user.getAttribute("email");
         String name = user.getAttribute("name");
